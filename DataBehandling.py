3be0e55f22f279f1f0c470880ecd40a81f2080c7
oskarfjo <oskarfjo@stud.ntnu.no>
2024-11-12 21:40:10 +0100

    justering for altitude data

--- DataBehandling.py
+++ DataBehandling.py
@@ -8,51 +8,52 @@ print("Current Working Directory:", os.getcwd()) # en pwd for pythonen. brukes t
 def extract_column_to_array(file_path, column_name):
     data_array = []
     row_count = 0
+    n = -1 # grense for hvor mange målinger man vil ta fra csv.
 
     with open(file_path, mode='r', newline='', encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile)
         
         for row in reader:
-            if row_count >= 210: # skal ta 210 - 10 målinger i et array
+            if n > 0 and row_count >= n + 10:
                 break
 
             if not row_count < 10: # skipper de første 10 målingene for å bare hente stabil data
                 data_array.append(float(row[column_name]))
-
             row_count += 1
 
     return data_array
 
-def calculate_distance(): # avstand mellom max og min verdi i meter
+def calculate_distance(array): # avstand mellom max og min verdi i meter
 
-    latitude = 62.4746318618
-    lat_to_m = 111320
+    #latitude = 62.4746318618
+    #lat_to_m = 111320
     
-    lat_range = max(lat_array) - min(lat_array)
-    lon_range = max(lon_array) - min(lon_array)
+    #lat_range = max(lat_array) - min(lat_array)
+    #lon_range = max(lon_array) - min(lon_array)
+    alt_range = max(array) - min(array)
     
-    lon_to_m = lat_to_m * math.cos(math.radians(latitude))
+    #lon_to_m = lat_to_m * math.cos(math.radians(latitude))
 
-    lat_distance = lat_range * lat_to_m
-    lon_distance = lon_range * lon_to_m
+    #lat_distance = lat_range * lat_to_m
+    #lon_distance = lon_range * lon_to_m
     
-    return lat_distance, lon_distance
+    return alt_range #lat_distance, lon_distance
 
-x = np.linspace(1, 201, 200)
 
-def plot_coordinates():
-    y_range, x_range = calculate_distance()
-    plt.figure(figsize=(x_range, y_range))
-    plt.scatter(lat_array, lon_array, c='blue', marker='o', label='Coordinates')
-    plt.plot(lat_av, lon_av, c='red', marker='x', label='Average')
-    plt.title("GPS Coordinates")
-    plt.xlabel("Longitude")
-    plt.ylabel("Latitude")
+
+def plot_coordinates(vec1, vec2):
+    x1 = np.linspace(1, len(vec1) + 1, len(vec1))
+    x2 = np.linspace(1, len(vec2) + 1, len(vec2))
+    plt.scatter(x2, vec2, c='blue', marker='o', label='Coordinates - iPhone')
+    plt.scatter(x1, vec1, c='red', marker='s', label='Coordinates - Arduino')
+    plt.title("Altitude plot - mix")
+    plt.xlabel("måling nr.")
+    plt.ylabel("moh")
     plt.legend()
     plt.grid(True)
     plt.show()
 
-def average(array): # gjennomsnitt
+def av(array): # gjennomsnitt
     n = len(array)
     sum = 0
     for i in range(n):
@@ -60,21 +61,20 @@ def average(array): # gjennomsnitt
 
     return sum/n
 
-def e_varians(array): # empirisk varians
+def var(array): # varians
     n = len(array)
-    av = average(array)
+    average = av(array)
     sum = 0
     for i in range(n):
-        sum += (array[i] - av)**2
+        sum += (array[i] - average)**2
 
     return sum/(n-1)
 
 
 ### File path til csv'en ; må endres for ny fil eller pc ###
-#file_path = './skolen/skolen_god_antenne.csv'
-#file_path = './skolen/RawData.csv'
-#file_path = './fotballbane/fotballbane_god_antenne.csv'
-file_path = './fotballbane/RawData.csv'
+arduino_path = './skolen/skolen_god_antenne.csv'
+iphone_path = './skolen/RawData.csv'
+
 
 ### Navn på kolonne som skal indexes ###
 lon_column = 'Longitude'
@@ -82,28 +82,40 @@ lat_column = 'Latitude'
 alt_column = 'Altitude'
 
 ### Henter ut dataene som skal brukes i oppgaven ###
-lon_array: list[float] = extract_column_to_array(file_path, lon_column)
-lat_array: list[float] = extract_column_to_array(file_path, lat_column)
-alt_array: list[float] = extract_column_to_array(file_path, alt_column)
-
+#lon_array: list[float] = extract_column_to_array(file_path, lon_column)
+#lat_array: list[float] = extract_column_to_array(file_path, lat_column)
+alt_array_ard: list[float] = extract_column_to_array(arduino_path, alt_column)
+alt_array_iph = extract_column_to_array(iphone_path, alt_column)
 ### Gjennomsnitt av dataene ###
-lon_av: float = average(lon_array)
-lat_av: float = average(lat_array)
-alt_av: float = average(alt_array)
+#lon_av: float = average(lon_array)
+#lat_av: float = average(lat_array)
+alt_av_ard: float = av(alt_array_ard)
+alt_av_iph: float = av(alt_array_iph)
 
 ### Varians av dataene ###
-lon_var: float = e_varians(lon_array)
-lat_var: float = e_varians(lat_array)
-alt_var: float = e_varians(alt_array)
+#lon_var: float = e_varians(lon_array)
+#lat_var: float = e_varians(lat_array)
+alt_var_ard: float = var(alt_array_ard)
+alt_var_iph: float = var(alt_array_iph)
 
-lat_range, lon_range = calculate_distance()
+alt_range_ard = calculate_distance(alt_array_ard)
+alt_range_iph = calculate_distance(alt_array_iph)
 
 ### Printer data i konsoll ###
-print(f'n = {len(alt_array)}')
-print(f'gjennomsnitt = lat: {lat_av}, lon: {lon_av}, alt: {alt_av}')
-print(f'varians = lat: {lat_var}, lon: {lon_var}, alt: {alt_var}')
-print(f'lat_max: {max(lat_array)}, lat_min: {(min(lat_array))}')
-print(f'lon_max: {max(lon_array)}, l')
-print(f'lat intervall = [{min(lat_array)}, {max(lat_array)}] ; {lat_range: .6f}m, lon intervall = [{min(lon_array)}, {max(lon_array)}] ; {lon_range: .6f}m')
+print('**** Arduino ****')
+print(f'n = {len(alt_array_ard)}')
+print(f'gjennomsnitt = alt: {alt_av_ard: .2f}moh')
+print(f'varians = alt: {alt_var_ard: .2f}m^2')
+print(f'alt intervall = [{min(alt_array_ard): .2f}, {max(alt_array_ard): .2f}] ; {alt_range_ard: .2f}m')
+print(f'S = {np.sqrt(alt_var_ard): .2f}m')
+print(f'SE(x) :) = {(np.sqrt(alt_var_ard)) / (np.sqrt(len(alt_array_ard))): .2f}m')
+print(' ')
+print('**** iPhone ****')
+print(f'n = {len(alt_array_iph)}')
+print(f'gjennomsnitt = alt: {alt_av_iph: .2f}moh')
+print(f'varians = alt: {alt_var_iph: .2f}m^2')
+print(f'alt intervall = [{min(alt_array_iph): .2f}, {max(alt_array_iph): .2f}] ; {alt_range_iph: .2f}m')
+print(f'S = {np.sqrt(alt_var_iph): .2f}m')
+print(f'SE(x) :) = {(np.sqrt(alt_var_iph)) / (np.sqrt(len(alt_array_iph))): .2f}m')
 
-#plot_coordinates()
+plot_coordinates(alt_array_ard, alt_array_iph)
